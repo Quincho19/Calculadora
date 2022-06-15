91b76df46dede7216dfda499b616bb2e11fbdedf
Joaquin Monzón <105814612+Quincho19@users.noreply.github.com>
2022-06-14 22:11:30 -0600

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,19 @@
+class Calculadora{
+    sumar(num1, num2){
+    return num1 + num2;
+    }
+
+    restar(num1, num2){
+    return num1 - num2;
+        }
+
+    dividr(num1, num2){
+    return num1 / num2;
+            }
+
+    multiplicar(num1, num2){
+    return num1 * num2;
+    }
+
+
+}
